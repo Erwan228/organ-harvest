9b5dcb10e95e092926a136ed42dd74a9555c2b00
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-03-24 15:10:14 +0100

    h

---
+++ Organ donor/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Organ_donor
+{
+    internal class Sykehus
+    {
+        public class Pasient
+        {
+            public string Name { get; private set; }
+            public int SunneNyrer { get; private set; }
+            public Pasient(string name, int sunneNyrer)
+            {
+                Name = name;
+                SunneNyrer = sunneNyrer;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            var person1 = new Pasient("Bernt", 0);
+            var person2 = new Pasient("kåte Kåre", 2);
+            Console.WriteLine(person1.Name + " har " + person1.SunneNyrer + " sunne nyrer.");
+            Console.WriteLine(person2.Name + " har " + person2.SunneNyrer + " sunne nyrer.");
+
+        }
+
+    }
+}
